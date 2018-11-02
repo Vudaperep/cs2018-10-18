5d4a613ee4a13e78e5bd7c179df2ff9da2462098
Vudaperep <emilk196@gmail.com>
2018-11-02 11:15:51 +0200

    Lesson 5 All complite

--- src/by/it/kvasov/lesson04/TaskC3.java
+++ src/by/it/kvasov/lesson04/TaskC3.java
@@ -26,214 +26,187 @@ package by.it.kvasov.lesson04;
 import java.util.Scanner;
 
 
-/*public static void main(String[] args){
-        for(int i=1;i<=10;i++){
-        for(int j=1;j<=10;j++){
-        switch(i){
-        case 1:
-        System.out.print("один ");
-        break;
-        case 2:
-        System.out.print("два ");
-        break;
-        case 3:
-        System.out.print("три ");
-        break;
-        case 4:
-        System.out.print("четыре ");
-        break;
-        case 5:
-        System.out.print("пять ");
-        break;
-        case 6:
-        System.out.print("шесть ");
-        break;
-        case 7:
-        System.out.print("семь ");
-        break;
-        case 8:
-        System.out.print("восемь ");
-        break;
-        case 9:
-        System.out.print("девять ");
-        break;
-        case 10:
-        System.out.print("десять ");
-        break;
+    public class TaskC3 {
+        public static void main(String[] args) {
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    switch (i) {
+                        case 1:
+                            System.out.print("один ");
+                            break;
+                        case 2:
+                            System.out.print("два ");
+                            break;
+                        case 3:
+                            System.out.print("три ");
+                            break;
+                        case 4:
+                            System.out.print("четыре ");
+                            break;
+                        case 5:
+                            System.out.print("пять ");
+                            break;
+                        case 6:
+                            System.out.print("шесть ");
+                            break;
+                        case 7:
+                            System.out.print("семь ");
+                            break;
+                        case 8:
+                            System.out.print("восемь ");
+                            break;
+                        case 9:
+                            System.out.print("девять ");
+                            break;
+                        case 10:
+                            System.out.print("десять ");
+                            break;
+                    }
+                    System.out.print("умножить на ");
+                    switch (j) {
+                        case 1:
+                            System.out.print("один ");
+                            break;
+                        case 2:
+                            System.out.print("два ");
+                            break;
+                        case 3:
+                            System.out.print("три ");
+                            break;
+                        case 4:
+                            System.out.print("четыре ");
+                            break;
+                        case 5:
+                            System.out.print("пять ");
+                            break;
+                        case 6:
+                            System.out.print("шесть ");
+                            break;
+                        case 7:
+                            System.out.print("семь ");
+                            break;
+                        case 8:
+                            System.out.print("восемь ");
+                            break;
+                        case 9:
+                            System.out.print("девять ");
+                            break;
+                        case 10:
+                            System.out.print("десять ");
+                            break;
+                    }
+                    System.out.print("равно ");
+                    if ((i * j) / 10 < 2) switch (i * j) {
+                        case 1:
+                            System.out.println("один");
+                            break;
+                        case 2:
+                            System.out.println("два");
+                            break;
+                        case 3:
+                            System.out.println("три");
+                            break;
+                        case 4:
+                            System.out.println("четыре");
+                            break;
+                        case 5:
+                            System.out.println("пять");
+                            break;
+                        case 6:
+                            System.out.println("шесть");
+                            break;
+                        case 7:
+                            System.out.println("семь");
+                            break;
+                        case 8:
+                            System.out.println("восемь");
+                            break;
+                        case 9:
+                            System.out.println("девять");
+                            break;
+                        case 10:
+                            System.out.println("десять");
+                            break;
+                        case 12:
+                            System.out.println("двенадцать");
+                            break;
+                        case 14:
+                            System.out.println("четырнадцать");
+                            break;
+                        case 15:
+                            System.out.println("пятнадцать");
+                            break;
+                        case 16:
+                            System.out.println("шестнадцать");
+                            break;
+                        case 18:
+                            System.out.println("восемнадцать");
+                            break;
+                    }
+                    else {
+                        switch ((i * j) / 10) {
+                            case 2:
+                                System.out.print("двадцать ");
+                                break;
+                            case 3:
+                                System.out.print("тридцать ");
+                                break;
+                            case 4:
+                                System.out.print("сорок ");
+                                break;
+                            case 5:
+                                System.out.print("пятьдесят ");
+                                break;
+                            case 6:
+                                System.out.print("шестьдесят ");
+                                break;
+                            case 7:
+                                System.out.print("семьдесят ");
+                                break;
+                            case 8:
+                                System.out.print("восемьдесят ");
+                                break;
+                            case 9:
+                                System.out.print("девяносто ");
+                                break;
+                            case 10:
+                                System.out.print("сто");
+                                break;
+                        }
+                        switch ((i * j) % 10) {
+                            case 0:
+                                System.out.println();
+                                break;
+                            case 1:
+                                System.out.println("один ");
+                                break;
+                            case 2:
+                                System.out.println("два ");
+                                break;
+                            case 3:
+                                System.out.println("три ");
+                                break;
+                            case 4:
+                                System.out.println("четыре ");
+                                break;
+                            case 5:
+                                System.out.println("пять ");
+                                break;
+                            case 6:
+                                System.out.println("шесть ");
+                                break;
+                            case 7:
+                                System.out.println("семь ");
+                                break;
+                            case 8:
+                                System.out.println("восемь ");
+                                break;
+                            case 9:
+                                System.out.println("девять ");
+                                break;
+                        }
+                    }
+                }
+            }
         }
-        dig(i);
-        System.out.print("умножить на ");
-        switch(j){
-        case 1:
-        System.out.print("один ");
-        break;
-        case 2:
-        System.out.print("два ");
-        break;
-        case 3:
-        System.out.print("три ");
-        break;
-        case 4:
-        System.out.print("четыре ");
-        break;
-        case 5:
-        System.out.print("пять ");
-        break;
-        case 6:
-        System.out.print("шесть ");
-        break;
-        case 7:
-        System.out.print("семь ");
-        break;
-        case 8:
-        System.out.print("восемь ");
-        break;
-        case 9:
-        System.out.print("девять ");
-        break;
-        case 10:
-        System.out.print("десять ");
-        break;
-        }
-        dig(j);
-        System.out.print("равно ");
-        if((i*j)/10<2)switch(i*j){
-        case 1:
-        System.out.println("один");
-        System.out.print("один");
-        break;
-        case 2:
-        System.out.println("два");
-        System.out.print("два");
-        break;
-        case 3:
-        System.out.println("три");
-        System.out.print("три");
-        break;
-        case 4:
-        System.out.println("четыре");
-        System.out.print("четыре");
-        break;
-        case 5:
-        System.out.println("пять");
-        System.out.print("пять");
-        break;
-        case 6:
-        System.out.println("шесть");
-        System.out.print("шесть");
-        break;
-        case 7:
-        System.out.println("семь");
-        System.out.print("семь");
-        break;
-        case 8:
-        System.out.println("восемь");
-        System.out.print("восемь");
-        break;
-        case 9:
-        System.out.println("девять");
-        System.out.print("девять");
-        break;
-        case 10:
-        System.out.println("десять");
-        System.out.print("десять");
-        break;
-        case 12:
-        System.out.println("двенадцать");
-        System.out.print("двенадцать");
-        break;
-        case 14:
-        System.out.println("четырнадцать");
-        System.out.print("четырнадцать");
-        break;
-        case 15:
-        System.out.println("пятнадцать");
-        System.out.print("пятнадцать");
-        break;
-        case 16:
-        System.out.println("шестнадцать");
-        System.out.print("шестнадцать");
-        break;
-        case 18:
-        System.out.println("восемнадцать");
-        System.out.print("восемнадцать");
-        break;
-        }
-        else{
-@ @ -170,40+108,45@@public static void main(String[]args){
-        System.out.print("сто");
-        break;
-        }
-        switch((i*j)%10){
-        case 0:
-        System.out.println();
-        break;
-        case 1:
-        System.out.println("один ");
-        break;
-        case 2:
-        System.out.println("два ");
-        break;
-        case 3:
-        System.out.println("три ");
-        break;
-        case 4:
-        System.out.println("четыре ");
-        break;
-        case 5:
-        System.out.println("пять ");
-        break;
-        case 6:
-        System.out.println("шесть ");
-        break;
-        case 7:
-        System.out.println("семь ");
-        break;
-        case 8:
-        System.out.println("восемь ");
-        break;
-        case 9:
-        System.out.println("девять ");
-        break;
-        dig((i*j)%10);
-        }
-        System.out.println();
-        }
-        }
-        }
-public static void dig(int a){
-        switch(a){
-        case 1:
-        System.out.print("один ");
-        break;
-        case 2:
-        System.out.print("два ");
-        break;
-        case 3:
-        System.out.print("три ");
-        break;
-        case 4:
-        System.out.print("четыре ");
-        break;
-        case 5:
-        System.out.print("пять ");
-        break;
-        case 6:
-        System.out.print("шесть ");
-        break;
-        case 7:
-        System.out.print("семь ");
-        break;
-        case 8:
-        System.out.print("восемь ");
-        break;
-        case 9:
-        System.out.print("девять ");
-        break;
-        case 10:
-        System.out.print("десять ");
-        break;
-        }
-        }
-        }
-*/
+    }
